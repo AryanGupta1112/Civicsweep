0a24dd4b7ab676eaf5837ca7de027f7939e8adc4
Aryan Gupta <91181333+AryanGupta1112@users.noreply.github.com>
2026-02-13 06:19:22 +0530

    Add app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.smartwaste"
+    compileSdk = 36   // use stable 34
+
+    defaultConfig {
+        applicationId = "com.example.smartwaste"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 5
+        versionName = "5.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildFeatures { viewBinding = true }
+
+    packaging { resources.excludes += "/META-INF/{AL2.0,LGPL2.1}" }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("com.google.android.gms:play-services-maps:19.2.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation("org.osmdroid:osmdroid-android:6.1.20")
+    implementation("org.osmdroid:osmdroid-wms:6.1.20")
+    implementation("androidx.webkit:webkit:1.14.0") // or latest stable version
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+}
